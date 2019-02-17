c88536d4c169544b38762a4778bfe2ada794e1b7
Unknown <phoenix@risen.solutions>
2019-02-18 09:36:06 +1100

    Simple Unit Tests

---
+++ app/app/calc.py
@@ -0,0 +1,20 @@
+
+
+def add(x, y):
+    """
+    Add two numbers together
+    :param x: Number 1
+    :param y: Number 2
+    :return: Sum of number 1 & 2
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtract x from y
+    :param x: number to subtract
+    :param y: number to subtract from
+    :return: result
+    """
+    return y - x
