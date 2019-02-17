f79a25ac2d045591cf110509f18d3f161e6459b8
Unknown <phoenix@risen.solutions>
2019-02-18 10:29:14 +1100

    Setup Create Super User function in custom model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'Test123')
+
+    def test_create_new_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@risen.solutions',
+            'Test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
